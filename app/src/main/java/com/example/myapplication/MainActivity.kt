7ec6de747e318dc874563d1c0f17ccd590533412
Dmitry <dima.gotovko.sport@mail.ru>
2022-05-02 10:54:48 +0300

    My Application List

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.TextView
 import com.github.javafaker.Faker
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    private lateinit var listContainer: LinearLayout
+    private lateinit var listView: ListView
 
     private val controller = MainController()
 
@@ -17,40 +19,18 @@ class MainActivity : AppCompatActivity(), MainView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listContainer = findViewById(R.id.listContainer)
+        listView = findViewById(R.id.list_view)
 
         controller.onViewReady(this)
     }
 
     override fun displayList(users: List<User>) {
-        listContainer.removeAllViews()
-
-        users.forEach() {
-            val itemView = createItemView(listContainer)
-            listContainer.addView(itemView)
-
-            val fullName = it.firstName + "" + it.lastName
-            fillItemView(itemView, fullName, it.address)
-        }
-    }
-
-    private fun createItemView(listContainer: LinearLayout): View {
-        return layoutInflater.inflate(R.layout.list_item, listContainer, false)
-    }
-
-    private fun fillItemView(itemView: View, fullName: String?, fullAddress: String?) {
-        val userFullNameTextView = itemView.findViewById<TextView>(R.id.userFullNameTextView)
-        val userAddressTextView = itemView.findViewById<TextView>(R.id.userAddressTextView)
-
-        userFullNameTextView.text = fullName
-        userAddressTextView.text = fullAddress
+        val newAdapter = UsersListAdapter(layoutInflater, users)
+        listView.adapter = newAdapter
     }
 
     fun addListItem(view: View?) = controller.onAddRandomClicked()
-
     fun removeLast(view: View) = controller.onRemoveLastClicked()
-
     fun removeAll(view: View) = controller.onRemoveAllClicked()
-
     fun editSecond(view: View) = controller.onEditSecondClicked()
 }
